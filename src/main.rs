a35bee4daaa83f3e7f956c56e13dcc0ded4e2b18
Jonathan Cobb <jonathan@kyuss.org>
2020-09-04 19:33:44 -0400

    add file headers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 #![deny(warnings)]
+/**
+ * Copyright (c) 2020 Bubble, Inc.  All rights reserved.
+ * For personal (non-commercial) use, see license: https://getbubblenow.com/bubble-license/
+ */
 
 extern crate lru;
 
