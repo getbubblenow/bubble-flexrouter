a2024dc96158eaaaba9acfa19c10d8a15b868cc1
Jonathan Cobb <jonathan@kyuss.org>
2020-09-05 13:33:43 -0400

    WIP. registering with bubble

--- src/admin.rs
+++ src/admin.rs
@@ -4,9 +4,15 @@
  * For personal (non-commercial) use, see license: https://getbubblenow.com/bubble-license/
  */
 
-extern crate lru;
+extern crate rand;
 
 use std::net::SocketAddr;
+
+use rand::Rng;
+use rand::distributions::Alphanumeric;
+
+use reqwest;
+use reqwest::StatusCode as ReqwestStatusCode;
 
 use serde_derive::{Deserialize, Serialize};
 
@@ -16,19 +22,30 @@ use warp::{Filter};
 use crate::pass::is_correct_password;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
-struct Registration {
+struct AdminRegistration {
     password: String,
     session: String,
     bubble: String
 }
 
-pub async fn start_admin (admin_port: u16, hashed_password : String) {
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct BubbleRegistration {
+    auth_token: String,
+    port: u16
+}
+
+pub async fn start_admin (admin_port : u16,
+                          proxy_port : u16,
+                          hashed_password : String,
+                          session_id : String) {
     let admin_sock: SocketAddr = format!("127.0.0.1:{}", admin_port).parse().unwrap();
 
     let register = warp::path!("register")
         .and(warp::body::content_length_limit(1024 * 16))
         .and(warp::body::json())
+        .and(warp::any().map(move || proxy_port))
         .and(warp::any().map(move || hashed_password.clone()))
+        .and(warp::any().map(move || session_id.clone()))
         .and_then(handle_register);
 
     let routes = warp::post().and(register);
@@ -38,7 +55,12 @@ pub async fn start_admin (admin_port: u16, hashed_password : String) {
     server.await;
 }
 
-async fn handle_register(registration : Registration, hashed_password : String) -> Result<impl warp::Reply, warp::Rejection> {
+const HEADER_BUBBLE_SESSION: &'static str = "X-Bubble-Session";
+
+async fn handle_register(registration : AdminRegistration,
+                         proxy_port : u16,
+                         hashed_password : String,
+                         session_id : String) -> Result<impl warp::Reply, warp::Rejection> {
     let pass_result = is_correct_password(registration.password, hashed_password);
     if pass_result.is_err() {
         eprintln!("handle_register: error verifying password: {:?}", pass_result.err());
@@ -52,9 +74,53 @@ async fn handle_register(registration : Registration, hashed_password : String) 
             http::StatusCode::UNAUTHORIZED,
         ))
     } else {
-        Ok(warp::reply::with_status(
-            "Received registration JSON",
-            http::StatusCode::OK,
-        ))
+        // try to register with bubble
+
+        // create a random token
+        let token = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .collect::<String>();
+
+        // create the registration object
+        let bubble_registration = BubbleRegistration {
+            port: proxy_port,
+            auth_token: token
+        };
+
+        // PUT it and see if it worked
+        let client = reqwest::Client::new();
+        let url = format!("https://{}/me/flexRouters", registration.bubble);
+
+        match client.put(url.as_str())
+            .header(HEADER_BUBBLE_SESSION, session_id)
+            .json(&bubble_registration)
+            .send().await {
+            Ok(response) => {
+                match response.status() {
+                    ReqwestStatusCode::OK => {
+                        eprintln!("successfully registered with bubble");
+                        Ok(warp::reply::with_status(
+                            "successfully registered with bubble",
+                            http::StatusCode::OK,
+                        ))
+                    },
+                    _ => {
+                        eprintln!("error registering with bubble: {:?}", response.status());
+                        Ok(warp::reply::with_status(
+                            "error registering with bubble",
+                            http::StatusCode::PRECONDITION_FAILED,
+                        ))
+                    }
+                }
+            },
+            Err(error) => {
+                eprintln!("error registering with bubble: {:?}", error);
+                Ok(warp::reply::with_status(
+                    "error registering with bubble",
+                    http::StatusCode::PRECONDITION_FAILED,
+                ))
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
  * For personal (non-commercial) use, see license: https://getbubblenow.com/bubble-license/
  */
 
+use std::env;
 use std::process::exit;
 
 use clap::{Arg, ArgMatches, App};
@@ -17,13 +18,6 @@ use bubble_flexrouter::net::is_private_ip;
 use bubble_flexrouter::pass::init_password;
 use bubble_flexrouter::proxy::start_proxy;
 
-// To try this example:
-// 1. cargo run --example http_proxy
-// 2. config http_proxy in command line
-//    $ export http_proxy=http://127.0.0.1:8100
-//    $ export https_proxy=http://127.0.0.1:8100
-// 3. send requests
-//    $ curl -i https://www.some_domain.com/
 #[tokio::main]
 async fn main() {
     let args : ArgMatches = App::new("bubble-flexrouter")
@@ -76,39 +70,16 @@ async fn main() {
             .value_name("ENV_VAR_NAME")
             .help("environment variable containing the admin password. overwrites previous value")
             .takes_value(true))
+        .arg(Arg::with_name("session_env_var")
+            .short("s")
+            .long("session-env-var")
+            .value_name("ENV_VAR_NAME")
+            .help("environment variable containing the session ID")
+            .takes_value(true))
         .get_matches();
 
     // todo: ensure we are running as root (or Administrator on Windows)
     println!("\nThe ID of the current user is {}\n", whoami::username());
-
-    println!(
-        "User→Name      whoami::realname():    {}",
-        whoami::realname()
-    );
-    println!(
-        "User→Username  whoami::username():    {}",
-        whoami::username()
-    );
-    println!(
-        "Host→Name      whoami::devicename():  {}",
-        whoami::devicename()
-    );
-    println!(
-        "Host→Hostname  whoami::hostname():    {}",
-        whoami::hostname()
-    );
-    println!(
-        "Platform       whoami::platform():    {}",
-        whoami::platform()
-    );
-    println!(
-        "OS Distro      whoami::distro():      {}",
-        whoami::distro()
-    );
-    println!(
-        "Desktop Env.   whoami::desktop_env(): {}",
-        whoami::desktop_env()
-    );
 
     let password_file_opt = args.value_of("password_file");
     if password_file_opt.is_none() {
@@ -119,6 +90,27 @@ async fn main() {
 
     let password_opt = args.value_of("password_env_var");
     let password_hash = init_password(password_file, password_opt);
+
+    let session_opt = args.value_of("session_env_var");
+    if session_opt.is_none() {
+        eprintln!("\nERROR: session-env-var argument is required\n");
+        exit(2);
+    }
+    let session_env_var = session_opt.unwrap();
+    if session_env_var.len() == 0 {
+        eprintln!("\nERROR: session-env-var was empty\n");
+        exit(2);
+    }
+    let session_env_var_result = env::var(session_env_var);
+    if session_env_var_result.is_err() {
+        eprintln!("\nERROR: session-env-var argument was {} but that environment variable was not defined\n", session_env_var);
+        exit(2);
+    }
+    let session_id = session_env_var_result.unwrap();
+    if session_id.trim().len() == 0 {
+        eprintln!("\nERROR: session-env-var argument was {} but the value of that environment variable was empty\n", session_env_var);
+        exit(2);
+    }
 
     let proxy_ip_opt = args.value_of("proxy_ip");
     if proxy_ip_opt.is_none() {
@@ -148,10 +140,11 @@ async fn main() {
     }
 
     let admin_port = args.value_of("admin_port").unwrap().parse::<u16>().unwrap();
-    start_admin(admin_port, password_hash).await;
-
     let dns1_ip = args.value_of("dns1").unwrap();
     let dns2_ip = args.value_of("dns2").unwrap();
     let proxy_port = args.value_of("proxy_port").unwrap().parse::<u16>().unwrap();
+
+    start_admin(admin_port, proxy_port, password_hash, session_id).await;
+
     start_proxy(dns1_ip, dns2_ip, proxy_bind_addr.unwrap().ip(), proxy_port);
 }
