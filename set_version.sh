4ccf6b9fa2fe40932f565ff5dadd707c76efd344
Jonathan Cobb <jonathan@kyuss.org>
2020-09-09 14:42:41 -0400

    make set_version.sh work on mac

--- set_version.sh
+++ set_version.sh
@@ -7,4 +7,5 @@ if [[ -z "${BUILD_NUMBER}" ]] ; then
   echo "No BUILD_NUMBER environment variable was set"
   exit 1
 fi
-sed -i "s/.DEV_BUILD/\.${BUILD_NUMBER}/" src/version.rs
+cp src/version.rs  src/version.rs.orig
+cat src/version.rs.orig | sed "s/.DEV_BUILD/\.${BUILD_NUMBER}/" > src/version.rs
