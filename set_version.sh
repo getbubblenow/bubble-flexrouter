f991cf5bf93e670d7c07c9cd3504cd8c4b40ef47
Jonathan Cobb <jonathan@kyuss.org>
2020-09-09 14:28:37 -0400

    make set_version.sh work on mac

--- set_version.sh
+++ set_version.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#
+# Called by jenkins to update version.rs with the current build number
+#
 
 if [[ -z "${BUILD_NUMBER}" ]] ; then
   echo "No BUILD_NUMBER environment variable was set"
   exit 1
 fi
-sed -i "s/.DEV_BUILD/.${BUILD_NUMBER}/" src/version.rs
+sed -i "s/.DEV_BUILD/\.${BUILD_NUMBER}/" src/version.rs
