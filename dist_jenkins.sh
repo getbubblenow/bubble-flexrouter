fa8d06efbf4a84807aba8dc32755149b5d26682e
Jonathan Cobb <jonathan@kyuss.org>
2020-09-20 11:34:23 -0400

    make jenkins home explicit

--- dist_jenkins.sh
+++ dist_jenkins.sh
@@ -2,27 +2,29 @@
 
 FLEX_PROJECT=${1:?no project name specified}
 
+JENKINS_HOME="$(cd ~jenkins && pwd)"
+
 function die () {
   echo 1>&2 "${1}"
   exit 1
 }
 
-LATEST_BUILD="$(find ~/jobs/${FLEX_PROJECT}/builds -maxdepth 1 -mindepth 1 -type d | xargs -n 1 basename  | sort -nr | head -1)"
+LATEST_BUILD="$(find ${JENKINS_HOME}/jobs/${FLEX_PROJECT}/builds -maxdepth 1 -mindepth 1 -type d | xargs -n 1 basename  | sort -nr | head -1)"
 if [[ -z "${LATEST_BUILD}" ]] ; then
   die "No latest build found"
 fi
 
-LATEST_VERSION="$(find ~/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/dist/releases/bubble-flexrouter/${FLEX_PROJECT} -maxdepth 1 -mindepth 1 -type d | sort -nr | head -1 | xargs -n 1 basename)"
+LATEST_VERSION="$(find ${JENKINS_HOME}/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/dist/releases/bubble-flexrouter/${FLEX_PROJECT} -maxdepth 1 -mindepth 1 -type d | sort -nr | head -1 | xargs -n 1 basename)"
 if [[ -z "${LATEST_VERSION}" ]] ; then
   die "No latest version found"
 fi
 
-LATEST_ZIP="$(find ~/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/dist/releases/bubble-flexrouter/${FLEX_PROJECT}/${LATEST_VERSION} -maxdepth 1 -mindepth 1 -type f -name "bubble-flexrouter.zip" | head -1)"
+LATEST_ZIP="$(find ${JENKINS_HOME}/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/dist/releases/bubble-flexrouter/${FLEX_PROJECT}/${LATEST_VERSION} -maxdepth 1 -mindepth 1 -type f -name "bubble-flexrouter.zip" | head -1)"
 if [[ -z "${LATEST_ZIP}" ]] ; then
   die "No latest zip found"
 fi
 
-RELEASE_TOP="~jenkins/public/public/releases/bubble-flexrouter/${FLEX_PROJECT}/"
+RELEASE_TOP="${JENKINS_HOME}/public/public/releases/bubble-flexrouter/${FLEX_PROJECT}/"
 RELEASE_DIR="${RELEASE_TOP}/${LATEST_VERSION}"
 
 mkdir -p ${RELEASE_DIR} || die "Error creating release dir: ${RELEASE_DIR}"
