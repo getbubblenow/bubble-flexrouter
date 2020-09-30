49f3552e05142586626aa20440d4d4a3d4d20e14
Jonathan Cobb <jonathan@kyuss.org>
2020-09-30 14:16:08 -0400

    WIP. building standalone installer for mac

--- dist_jenkins.sh
+++ dist_jenkins.sh
@@ -32,8 +32,8 @@ if [[ -z "${LATEST_ZIP}" ]] ; then
 fi
 
 if [[ ${IS_MACOS} -eq 1 ]] ; then
-  LATEST_INSTALL_SH="$(find ${JENKINS_HOME}/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/dist/releases/bubble-flexrouter/${FLEX_PROJECT}/${LATEST_VERSION} -maxdepth 1 -mindepth 1 -type f -name "install.sh" | head -1)"
-  if [[ -z "${LATEST_INSTALL_SH}" ]] ; then
+  LATEST_INSTALL_SH="${JENKINS_HOME}/jobs/${FLEX_PROJECT}/builds/${LATEST_BUILD}/archive/install.sh"
+  if [[ ! -s "${LATEST_INSTALL_SH}" ]] ; then
     die "No install.sh found"
   fi
 fi
